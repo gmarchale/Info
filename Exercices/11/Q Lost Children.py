c9aa0bdc1b026c8ba8134b878b5fae7d49d75e19
Robin Verschuren <robin.ver@hotmail.com>
2019-01-14 15:50:18 +0100

    Update Q Lost Children.py

--- Exercices/11/Q Lost Children.py
+++ Exercices/11/Q Lost Children.py
@@ -1,8 +1,20 @@
-#Wiaux Bastien
-
+# -------------------------------------------
+#   Solution par Wiaux Bastien ( @wiauxb )
+# -------------------------------------------
 child = first_child
 while child.next_child() != first_child:
     if not child.is_next_valid():
         return False
     child = child.next_child()
 return True
+
+# -------------------------------------------
+#   Solution par ( @rverschuren ) 
+# -------------------------------------------
+current = first_child
+
+while current.is_next_valid():
+    current = current.next_child()
+    if current is first_child:
+        return True
+return False
